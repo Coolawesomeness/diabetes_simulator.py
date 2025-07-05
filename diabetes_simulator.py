3555918e1135b2b89f40610bcde29cc6646e93ec
Siddharth Tirumalai <86448541+Coolawesomeness@users.noreply.github.com>
2025-07-05 17:22:55 -0500

    Update diabetes_simulator.py

--- diabetes_simulator.py
+++ diabetes_simulator.py
@@ -2,7 +2,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 from random import uniform
     
-st.title=("Siddharth Tirumalai - Diabetes Simulator")
+st.title("Siddharth Tirumalai - Diabetes Simulator")
 st.write("Use the sliders and survey to simulate how lifestyle, medication, and diet impact glucose levels.")
 
 # Health status selection
